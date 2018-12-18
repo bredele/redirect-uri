1e17dcb31ee4eb0a23ca444e4944a4a6ce256ba2
bredele <olivier.wietrich@gmail.com>
2018-12-18 11:04:31 -0700

    should not validate HTTP urls

--- test/redirect-uri.js
+++ test/redirect-uri.js
@@ -4,3 +4,8 @@
 
 const test = require('tape')
 const validate = require('..')
+
+test('should not validate http urls', assert => {
+  assert.plan(1)
+  assert.equal(validate('http://www.helloworld.com') == null, true)
+})
