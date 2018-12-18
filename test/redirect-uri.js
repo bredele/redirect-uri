0498d71d7fc4923bd13a7001b237ccaa7eb2d1f8
bredele <olivier.wietrich@gmail.com>
2018-12-18 11:04:58 -0700

    should validate https url

--- test/redirect-uri.js
+++ test/redirect-uri.js
@@ -9,3 +9,9 @@ test('should not validate http urls', assert => {
   assert.plan(1)
   assert.equal(validate('http://www.helloworld.com') == null, true)
 })
+
+
+test('should validate https urls', assert => {
+  assert.plan(1)
+  assert.equal(validate('https://www.helloworld.com'), 'https://www.helloworld.com')
+})
