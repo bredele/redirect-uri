5319a2fb29b17cc7e5ff96ba43026b06e5eb02ba
bredele <olivier.wietrich@gmail.com>
2018-12-18 11:37:34 -0700

    Add additional tests

--- test/redirect-uri.js
+++ test/redirect-uri.js
@@ -15,9 +15,27 @@ test('should validate HTTPS URI', assert => {
   assert.equal(validate('https://www.helloworld.com'), 'https://www.helloworld.com')
 })
 
+test('should vaidate HTTPS URI without path', assert => {
+  assert.plan(1)
+  assert.equal(validate('https://'), 'https://')
+})
+
+
+test('should not validate HTTPS URI without ://', assert => {
+  assert.plan(3)
+  assert.equal(validate('https') == null, true)
+  assert.equal(validate('https:') == null, true)
+  assert.equal(validate('https:/') == null, true)
+})
+
 test('should validate URI with protocols different than HTTP', assert => {
   assert.plan(1)
   assert.equal(validate('demoapp://www.helloworld.com'), 'demoapp://www.helloworld.com')
+})
+
+test('should validate native URI without path', assert => {
+  assert.plan(1)
+  assert.equal(validate('demoapp://'), 'demoapp://')
 })
 
 test('should not validate URIs without protocols', assert => {
