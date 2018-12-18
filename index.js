1e65e66511a78c2636da3d36e30e1fe01b2c0bb2
bredele <olivier.wietrich@gmail.com>
2018-12-18 11:10:49 -0700

    Does not validate URIs without protocols

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 module.exports = (url) => {
   const parse = url.split('://')
+  if (parse.length < 2) return
   if (parse[0] !== 'http') return url
 }
--- test/redirect-uri.js
+++ test/redirect-uri.js
@@ -5,13 +5,22 @@
 const test = require('tape')
 const validate = require('..')
 
-test('should not validate http urls', assert => {
+test('should not validate HTTP URI', assert => {
   assert.plan(1)
   assert.equal(validate('http://www.helloworld.com') == null, true)
 })
 
-
-test('should validate https urls', assert => {
+test('should validate HTTPS URI', assert => {
   assert.plan(1)
   assert.equal(validate('https://www.helloworld.com'), 'https://www.helloworld.com')
+})
+
+test('should validate URI with protocols different than HTTP', assert => {
+  assert.plan(1)
+  assert.equal(validate('demoapp://www.helloworld.com'), 'demoapp://www.helloworld.com')
+})
+
+test('should not validate URIs without protocols', assert => {
+  assert.plan(1)
+  assert.equal(validate('www.helloworld.com') == null, true)
 })
